d59ebb1c98332f76d7e1e3b8eda6e771f21f6454
mdmanik74 <manikmhamud74@outlook.com>
2023-03-26 00:29:37 +0600

    function return

---
+++ function return.dart
@@ -0,0 +1,41 @@
+
+void main(){
+
+  print (test());
+
+}
+
+String test(){
+
+  return "hellow world";
+
+}
+Console
+hellow world
+Documentation
+keyboard
+Privacy notice Send feedback
+no issues
+Based on Flutter 3.7.8 Dart SDK 2.19.5
+info
+
+
+
+
+void main(){
+  
+  List a =myfun();
+  print(a);
+}
+
+List myfun(){
+  var list = new List();
+  list.add("function return");
+  list.add("function return");
+  list.add("function return");
+  list.add("function return");
+  list.add("function return");
+  return list;
+}
+
+
---
+++ function.dart
@@ -0,0 +1,11 @@
+void main (){
+  
+  myfun();
+  
+}
+
+void myfun(){
+  int a=10;
+  int b = 20;
+  print(a+b);
+}
